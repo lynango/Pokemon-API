29bc9ce43dd08ebe9df62dc05944966899e8fdf8
Lyna Ngo <lynango5623@gmail.com>
2022-04-01 04:54:28 -0700

    First Commit

--- src/components/Pokemons.js
+++ src/components/Pokemons.js
@@ -1,19 +1,23 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Pokemons = (props) => {
-    const [pokemons, setPokemons] = useState([]);
+const [pokemons, setPokemons] = useState([]);
 
     useEffect(() => {
-        fetch('https://pokeapi.co/api/v2/pokemon')
-            .then(response => response.json())
-            .then(response => setPokemons(response.results))
-    }, []);
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=807")
+    .then((response) => {setPokemons(response.data.results)})}, 
+    []);
 
     return (
         <div>
-            {pokemons.length > 0 && pokemons.map((pokemon, index)=>{
-                return (<div key={index}>{pokemon.name}</div>)
-            })}
+            <ul>
+            {pokemons.map((pokemon, index) => (
+                <ul key={index}>
+                    {index + 1}{". "}{pokemon.name}
+            </ul>
+            ))}
+            </ul>
         </div>
     );
 }
